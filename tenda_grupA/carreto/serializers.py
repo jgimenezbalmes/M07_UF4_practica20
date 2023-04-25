39e512d5d64cb969c60f08ee2601e0fb0160c3f5
ksama22 <ksama22@jaumebalmes.net>
2023-04-25 20:59:03 +0200

    Create 'Carreto

---
+++ tenda_grupA/carreto/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+# Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+from .models import Carreto
+from .serializers import *
+
+class CarretoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Carreto
+        fields = ('idCarreto', 'productes')
+
+
+
+    
