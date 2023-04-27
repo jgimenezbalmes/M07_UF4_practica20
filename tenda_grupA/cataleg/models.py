ba7a196a5a4012b2889f7e3dc1f0e22ba9f084d2
unknown <jgimenez@jaumebalmes.net>
2023-04-27 19:22:37 +0200

    corregits errors de tipus de dada a model

--- tenda_grupA/cataleg/models.py
+++ tenda_grupA/cataleg/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class Producte(models.Model):
     idProducte = models.AutoField(auto_created=True, primary_key=True, serialize=False)
-    nom = models.IntegerField()
+    nom = models.CharField("nom", max_length=50)
     preu = models.DecimalField(max_digits=10, decimal_places=2)
     fabricant = models.CharField("fabricant", max_length=50)
-    origen = models.CharField("fabricant", max_length=50)
+    origen = models.CharField("origen", max_length=50)
     caducitat = models.DateField()
     descripcio = models.TextField()
 
