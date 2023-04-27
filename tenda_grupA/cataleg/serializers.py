757b5c8f36c923a30be151eed32e786e892cafc7
unknown <jgimenez@jaumebalmes.net>
2023-04-27 18:59:30 +0200

    fet actualitzar producte

--- tenda_grupA/cataleg/serializers.py
+++ tenda_grupA/cataleg/serializers.py
@@ -2,4 +2,7 @@ from rest_framework import serializers
 from .models import Producte
 
 class ProducteSerializer(serializers.ModelSerializer):
-    
+
+    class Meta:
+        model = Producte
+        fields = ('idProducte', 'nom', 'preu', 'fabricant', 'origen', 'caducitat', 'descripcio')
--- tenda_grupA/cataleg/views.py
+++ tenda_grupA/cataleg/views.py
@@ -16,4 +16,24 @@ def veure_tots(request):
     return Response(serializer.resposta)
 
 @api_view(['POST'])
-def afegir_producte(request, nomP)
+def afegir_producte(request, nomP, preuP, fabP, origenP, cadP, descP):
+    prod= Producte(nom = nomP, preu= preuP, fabricant=fabP, origen=origenP, caducitat=cadP, descripcio=descP)
+    prod.save()
+    return Response({'success': 'Producte creat amb exit.'}, status=status.HTTP_201_CREATED)
+
+@api_view(['PUT'])
+def actualitzar_producte(request, idP, nomP, preuP, fabP, origenP, cadP, descP):
+    try:
+        prod = Producte.objects.get(idProducte=idP)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    prod.nom = nomP
+    prod.preu = preuP
+    prod.fabricant = fabP
+    prod.origen = origenP
+    prod.caducitat = cadP
+    prod.descripcio = descP
+    prod.save()
+    serializer = ProducteSerializer(prod, context={'request': request})
+
+    return Response(serializer.data)
